5b214c6c5600c8bf15effde6c22a8a7804413272
odianbtw <iweef21@gmail.com>
2025-10-02 10:57:21 +0200

    Initialized project

---
+++ src/test/java/com/paradigma/filmio/FilmioApplicationTests.java
@@ -0,0 +1,15 @@
+package com.paradigma.filmio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class FilmioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/paradigma/filmio/TestFilmioApplication.java
@@ -0,0 +1,11 @@
+package com.paradigma.filmio;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestFilmioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(FilmioApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
